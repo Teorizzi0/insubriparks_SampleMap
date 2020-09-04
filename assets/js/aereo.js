9d5936a4578a5334527c1cf855d34907c42e3a61
Matteo Rizzi <matteo4.rizzi@mail.polimi.it>
2020-09-04 16:37:07 +0200

    Zoom level added

---
+++ assets/js/aereo.js
@@ -0,0 +1,11 @@
+let aereo = {
+"type": "FeatureCollection",
+"name": "aereo",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Aeroporto di Milano-Malpensa", "id": 1, "type": "airport" }, "geometry": { "type": "Point", "coordinates": [ 8.725199967792697, 45.628650530916808 ] } },
+{ "type": "Feature", "properties": { "name": "Aeroporto internazionale il Caravaggio di Bergamo-Orio al Serio", "id": 2, "type": "airport" }, "geometry": { "type": "Point", "coordinates": [ 9.705077143622486, 45.669038253241133 ] } },
+{ "type": "Feature", "properties": { "name": "Aeroporto di Lugano-Agno", "id": 3, "type": "airport" }, "geometry": { "type": "Point", "coordinates": [ 8.910193700857366, 46.00413724620141 ] } },
+{ "type": "Feature", "properties": { "name": "Aeroporto di Milano-Linate", "id": 4, "type": "airport" }, "geometry": { "type": "Point", "coordinates": [ 9.273995143025532, 45.451201274046028 ] } }
+]
+}
---
+++ assets/js/train.js
@@ -0,0 +1,10 @@
+let train = {
+"type": "FeatureCollection",
+"name": "train",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Chiasso", "id": 1 }, "geometry": { "type": "Point", "coordinates": [ 9.0316031, 45.832248 ] } },
+{ "type": "Feature", "properties": { "name": "Como San Giovanni", "id": 2 }, "geometry": { "type": "Point", "coordinates": [ 9.0727772, 45.8090266 ] } },
+{ "type": "Feature", "properties": { "name": "Varese", "id": 3 }, "geometry": { "type": "Point", "coordinates": [ 8.8329476, 45.8161996 ] } }
+]
+}
